dec4d2e41dbd7a175beee2bd62a14de385968778
DushyantBhardwaj22 <maniebhardwaj1@gmail.com>
2023-03-28 17:28:05 +0530

    added signin,signup components compete.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './component/form/SignIn/SignIn';
+import SignUp from './component/form/SignUp/SignUp';
+import Dashboard from './component/Dashboard/Dashboard';
+import Upload from './component/Upload/Upload';
+import VideoPlayer from './component/VideoPlayer/videoPlayer';
+import ExistingPage from './component/ExistingPage/ExistingPage'
+import MyVideos from './component/my-video/My-videos';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes>
+        <Route exact="true" path="/signin" element={<SignIn />} />
+        <Route exact="true" path="/signup" element={<SignUp />} />
+        <Route exact="true" path="/home" element={<Dashboard />} />
+        <Route exact="true" path="/" element={<ExistingPage />} />
+        <Route exact="true" path="/upload" element={<Upload />} />
+        <Route exact="true" path="/videos" element={<VideoPlayer />} />
+        <Route exact="true" path="/myvideos" element={<MyVideos />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
